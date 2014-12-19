81dd4834f40ebf7208ffba634b4e54008576e8f3
hasherezade <hasherezade@op.pl>
2014-12-19 17:44:14 +0100

    [FEATURE][cms_lister] Added: follow redirection

--- cms_lister.rb
+++ cms_lister.rb
@@ -13,6 +13,8 @@ require 'msf/core'
 class Metasploit3 < Msf::Auxiliary
   include Msf::Exploit::Remote::HttpClient
 
+  MAX_REDIR = 5
+
   def initialize(info={})
     super(update_info(info,
       'Name'           => "CMS File Lister",
@@ -32,6 +34,7 @@ class Metasploit3 < Msf::Auxiliary
       OptPath.new('PATH', [false, 'File with list of files to search']),
       OptString.new('TARGETURI', [true, 'URI of the lookup service', '/']),
       OptRegexp.new('NOT_FOUND', [true, '404 pattern', '/Not Found/']),
+      OptInt.new('REDIR_DEPTH', [true, "Redirection depth, max = #{MAX_REDIR}", 2]),
       OptRegexp.new('EXCLUDE_NAMES', [true, 'Skip files with names matching the pattern','\.(?:php|js|css|png|jpg|gif)$']),
       OptBool.new('DISPLAY_ERR', [true, 'Display error codes (if different than 404)', 'false']),
       OptBool.new('COMPARE_FILES', [true, "Compare remote file with local (Out: [+] - same; [-] - different; [?] - not compared)",'true'])
@@ -80,27 +83,14 @@ class Metasploit3 < Msf::Auxiliary
       end
       }
     my_file.close
-    print_status("Found paths: #{paths.length}")
+    print_status("Loaded #{paths.length} paths") if datastore['VERBOSE']
     return paths
   end
 
-  def process_page_error(url, res_code, res_body)
-    verbose = datastore['VERBOSE']
-    if verbose == false
-      return nil if res_code == 404
-    end
-
-    if datastore['DISPLAY_ERR']
-      print_error("#{url} : #{res_code}")
-    end
-  end
-
-  def get_url(path)
-    port = datastore['RPORT']
+  def get_url(path, is_ssl)
     proto = "http"
-    if (datastore['SSL'] == true)
-      proto = "https"
-    end
+    proto = "https" if is_ssl
+
     url = "#{proto}:/" + normalize_uri("#{datastore['RHOST']}//#{datastore['TARGETURI']}//#{path}")
   end
 
@@ -122,44 +112,64 @@ class Metasploit3 < Msf::Auxiliary
     return 0
   end
 
-  def path_search(path)
-    if not path
-      return nil
+  def cmpres_to_str(is_same)
+    verified = '[?]'
+    if is_same == 1
+      verified = '[+]'
+    elsif is_same == 0
+      verified = '[-]'
     end
+    return verified
+  end
 
-    port = datastore['RPORT']
-    old_ssl = autoenable_ssl(port)
-    url = get_url(path)
+  def path_search(path, url, port, redir_depth = 0)
+    return nil if not url
 
+    print_status("GET: #{url}") if (datastore['VERBOSE'])
+    autoenable_ssl(port)
     res = send_request_cgi({
         'method' => 'GET',
         'uri'    => url,
         'rhost' => datastore['RHOST'],
-        'rport' => datastore['RPORT']
+        'rport' => port
     })
-    datastore['SSL'] = old_ssl
 
-    if res and res.code == 200 and not res.body =~ datastore['NOT FOUND']
+    if not res
+      print_error("#{url}")
+      return false
+    end
+
+    if res.code == 404 or res.body =~ datastore['NOT FOUND']
+      print_error("Not found: #{res.code}") if datastore['VERBOSE'] and datastore['DISPLAY_ERR']
+      return false
+    end
+
+    if res.code == 200
       is_same = -1
       if datastore['COMPARE_FILES']
         is_same = compare_content(path, res.body)
       end
 
-      verified = '[?]'
-      if is_same == 1
-        verified = '[+]'
-      elsif is_same == 0
-        verified = '[-]'
-      end
-
+      verified = cmpres_to_str(is_same)
       print_good("#{verified} #{url}")
       return true
     end
-    if res
-      process_page_error(url, res.code, res.body)
-    else
-      print_error("#{url}")
+
+    if (res.code == 301 or res.code == 302) and res.redirection
+      if (redir_depth < MAX_REDIR and redir_depth < datastore['REDIR_DEPTH'])
+        redir_uri = res.redirection
+        is_ssl = false
+        if redir_uri.scheme == "https"
+          port = URI::HTTPS::DEFAULT_PORT
+          is_ssl = true
+        end
+        url = get_url(redir_uri.path, is_ssl)
+        print_status("Redirection to: #{url} : ERR: #{res.code}") if datastore['VERBOSE']
+        path_search(path, url, port, redir_depth + 1)
+      end
     end
+
+    print_error("#{url} : #{res.code}") if datastore['DISPLAY_ERR']
     return false
   end
 
@@ -178,7 +188,8 @@ class Metasploit3 < Msf::Auxiliary
       if skip_type(chunk)
         next
       end
-      pass = path_search(chunk)
+      url = get_url(chunk, datastore['SSL'])
+      pass = path_search(chunk, url, datastore['RPORT'])
       if pass
         found += 1
       end
@@ -188,9 +199,9 @@ class Metasploit3 < Msf::Auxiliary
 
   def autoenable_ssl(port)
     old_ssl = datastore['SSL']
-    if (port == 80)
+    if (port == URI::HTTP::DEFAULT_PORT)
       datastore['SSL'] = false
-    elsif (port == 443)
+    elsif (port == URI::HTTPS::DEFAULT_PORT)
       datastore['SSL'] = true
     end
     return old_ssl
@@ -221,6 +232,7 @@ class Metasploit3 < Msf::Auxiliary
     else
       print_error('CMS_DIR not set!')
     end
+
   end
 
 end
